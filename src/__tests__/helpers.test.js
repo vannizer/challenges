d1abd77cc8db9fe0bcfd956b542e9e4f90407187
Ham <pallop.chao@gmail.com>
2018-04-25 21:05:07 +0700

    Add unit tests

--- src/__tests__/helpers.test.js
+++ src/__tests__/helpers.test.js
@@ -1,7 +1,27 @@
-import { summaryDonations } from '../helpers'
+import { summaryDonations, isNotEmpty, add, toSlug } from '../helpers'
 
-describe('helpers', function() {
-  test('`summaryDonations` should calculate donations correctly', function() {
+describe('helpers', () => {
+  test('`summaryDonations` should calculate donations correctly', () => {
     expect(summaryDonations([1, 2, 3, 4])).toEqual(10)
+  })
+
+  test('`add` should calculate correctly', () => {
+    expect(add(1, 2, 3, 4)).toEqual(10)
+    expect(add(1, '2', 3, '4')).toEqual(10)
+  })
+
+  test('`isNotEmpty` should return true only `undefined` or `null`', () => {
+    expect(isNotEmpty({})).toBe(true)
+    expect(isNotEmpty(null)).toBe(false)
+    expect(isNotEmpty(undefined)).toBe(false)
+    expect(isNotEmpty(0)).toBe(true)
+    expect(isNotEmpty('')).toBe(true)
+  })
+
+  test('`toSlug` should handle it well', () => {
+    expect(toSlug('I am Batman')).toBe('i-am-batman')
+    expect(toSlug('   I    am    Batman   ')).toBe('i-am-batman')
+    expect(toSlug('I-AM-BATMAN')).toBe('i-am-batman')
+    expect(toSlug('I- -AM- -BATMAN')).toBe('i-am-batman')
   })
 })
---
+++ src/containers/__tests__/donate.test.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { render, Simulate } from 'react-testing-library'
+import CharityCard from '../CharityCard'
+
+test('calls toggle when press donate button', () => {
+  const handleToggle = jest.fn()
+  const { getByText } = render(<CharityCard onToggle={handleToggle} />)
+  const donateButton = getByText('donate', { selector: 'button' })
+
+  Simulate.click(donateButton)
+
+  expect(handleToggle).toHaveBeenCalledTimes(1)
+})
+
+test('pay button will disabled if not selected value', () => {
+  const { getByText, getByLabelText } = render(<CharityCard />)
+  const payButton = getByText('pay', { selector: 'button' })
+  const input10 = getByLabelText('10', { selector: 'input' })
+
+  expect(payButton.disabled).toBe(true)
+  Simulate.click(input10)
+  expect(payButton.disabled).toBe(false)
+})
+
+test('calls submit and toggle when press pay button', () => {
+  const handleToggle = jest.fn()
+  const handleSubmit = jest.fn()
+
+  const { getByLabelText, getByText } = render(
+    <CharityCard submit={handleSubmit} onToggle={handleToggle} />,
+  )
+  const payButton = getByText('pay', { selector: 'button' })
+  const input10 = getByLabelText('10', { selector: 'input' })
+
+  Simulate.click(input10)
+  Simulate.click(payButton)
+
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+  expect(handleToggle).toHaveBeenCalledTimes(1)
+})
